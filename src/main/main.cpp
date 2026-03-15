710e2b33a6432a71b21bd2803c5305a68d914c7b
Andhika Rahman <arputra.me@gmail.com>
2026-03-16 05:31:39 +0700

    test the snake

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,26 @@
+#include "coordinate/coordinate.h"
+#include "direction/direction.h"
+#include "snake/snake.h"
 #include <iostream>
 
 int main() {
     std::cout << "Hello CNAKE!\n";
+
+    Snake * snake = new Snake(5, {3, 6}, Direction::West);
+
+    std::cout << "\n";
+
+    std::cout << "snake length = " << snake->getLength() << "\n";
+    std::cout << "snake head = " << snake->getHead().x << " " << snake->getHead().y << "\n";
+    std::cout << "snake direction = " << (int)snake->getDirection() << "\n";
+    std::cout << "snake body: \n";
+    
+    for(auto [x,y] : snake->getBody()) {
+        std::cout << x << " " << y << "\n";
+    }
+
+    delete snake;
+
+    std::cout << "\n";
     return 0;
 }
