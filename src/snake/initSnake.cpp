187ae1230d79f0fda6795dbb6233bfd941d55ffa
Andhika Rahman <arputra.me@gmail.com>
2026-03-16 05:13:00 +0700

    rename len

--- src/snake/initSnake.cpp
+++ src/snake/initSnake.cpp
@@ -2,15 +2,15 @@
 #include "direction/direction.h"
 #include "snake/snake.h"
 
-Snake::Snake(int len, Coordinate head, Direction direction) {
+Snake::Snake(int length, Coordinate head, Direction direction) {
     this->direction = direction;
     this->head = head;
-    this->len = len;
+    this->length = length;
 
     int dx[] = {1, 0, -1, 0};
     int dy[] = {0, 1, 0, -1};
 
-    for(int i = 1; 1 <= len - 1; i++) {
+    for(int i = 1; 1 <= length - 1; i++) {
         Coordinate body = {head.x + i * dx[(int)direction],
                             head.y + i * dy[(int)direction]};
         
