9b095d864148b340f45d3713f39d17945f0be6e6
Andhika Rahman <arputra.me@gmail.com>
2026-03-16 13:20:46 +0700

    test snake movement

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,11 +6,24 @@
 int main() {
     Snake * snake = new Snake(5, {3, 6}, Direction::West);
 
-    std::cout << snake->getHead().x << " " << snake->getHead().y << "\n";
+    int direction;
 
-    for(auto [x,y] : snake->getBody()) {
-        std::cout << x << " " << y << "\n";
-    }
+    do {
+        std::cout << "\n";
+
+        std::cout << snake->getHead().x << " " << snake->getHead().y << "\n";
+
+        for(auto [x,y] : snake->getBody()) {
+            std::cout << x << " " << y << "\n";
+        }
+
+        std::cout << "Enter the direction (N=0, E=1, S=2, W=3), -1 to abort: ";
+        std::cin >> direction;
+
+        Direction newDirection = static_cast<Direction>(direction);
+
+        snake->move(newDirection);
+    } while(direction != -1);  
 
     delete snake;
     
