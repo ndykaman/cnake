1491a3d9230853fea3866eecd85c5d8ddc874032
Andhika Rahman <arputra.me@gmail.com>
2026-03-17 09:39:34 +0700

    optimizing the code

--- src/snake/moveSnake.cpp
+++ src/snake/moveSnake.cpp
@@ -2,36 +2,40 @@
 #include "direction/direction.h"
 #include "snake/snake.h"
 #include <cstdlib>
+#include <vector>
 
 void Snake::move(Direction newDirection) {
+    const Coordinate &curHead = this->head;
+    const Direction &curDirection = this->direction;
+    std::vector<Coordinate> &body = this->body;
     Coordinate newHead;
 
-    if(abs((int)(this->direction) - (int)newDirection) == 2) return;
+    if(abs((int)(curDirection) - (int)newDirection) == 2) return;
 
     switch ((int)newDirection) {
         case 0:
-            newHead.x = this->head.x - 1;
-            newHead.y = this->head.y;
+            newHead.x = curHead.x - 1;
+            newHead.y = curHead.y;
             break;
         case 1:
-            newHead.x = this->head.x;
-            newHead.y = this->head.y + 1;
+            newHead.x = curHead.x;
+            newHead.y = curHead.y + 1;
             break;
         case 2:
-            newHead.x = this->head.x + 1;
-            newHead.y = this->head.y;
+            newHead.x = curHead.x + 1;
+            newHead.y = curHead.y;
             break;
         case 3:
-            newHead.x = this->head.x;
-            newHead.y = this->head.y - 1;
+            newHead.x = curHead.x;
+            newHead.y = curHead.y - 1;
             break;
     }
 
     for(int i = this->length - 2; i > 0; i--) {
-        this->body[i] = this->body[i - 1];
+        body[i] = body[i - 1];
     }
 
-    this->body[0] = head;
+    body[0] = curHead;
     this->head = newHead;
     this->direction = newDirection;
 }
