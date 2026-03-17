0f986c6e300f4ca576b1dac9bb1515bf8fd1fcab
Andhika Rahman <arputra.me@gmail.com>
2026-03-17 09:30:55 +0700

    test the space

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,54 +1,16 @@
-#include "coordinate/coordinate.h"
-#include "direction/direction.h"
-#include "snake/snake.h"
-#include <cstdlib>
+#include "space/space.h"
 #include <iostream>
-#include <vector>
 
 int main() {
-    Snake * snake = new Snake(5, {3, 6}, Direction::West);
+    int r, c;
 
-    std::vector<std::vector<char>> grid(16, std::vector<char>(16, '.'));
+    std::cin >> r >> c;
 
-    char direction;
+    Space * space = new Space(r, c);
 
-    do {
-        system("clear");
-        std::cout << "\n";
+    space->print();
 
-        for(auto &vc : grid) {
-            for(auto &c : vc) {
-                c = '.';
-            }
-        }
-
-        grid[snake->getHead().x][snake->getHead().y] = '$';
-
-        for(auto [x,y] : snake->getBody()) {
-            grid[x][y] = '#';
-        }
-
-        for(auto vc : grid) {
-            for(auto c : vc) {
-                std::cout << c;
-            }
-            std::cout << "\n";
-        }
-
-        std::cout << "Enter the direction (n, e, s, w), q to quit: ";
-        std::cin >> direction;
-
-        Direction newDirection;
-
-        if(direction == 'n') newDirection = Direction::North;
-        else if (direction == 'e') newDirection = Direction::East;
-        else if(direction == 's') newDirection = Direction::South;
-        else if (direction == 'w') newDirection = Direction::West;
-
-        snake->move(newDirection);
-    } while(direction != 'q');  
-
-    delete snake;
+    delete space;
     
     return 0;
 }
