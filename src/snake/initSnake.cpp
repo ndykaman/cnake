4ac1452d94986458540071a422268685c583ceeb
Andhika Rahman <arputra.me@gmail.com>
2026-03-18 06:22:44 +0700

    add wrap-around mechanism

--- src/snake/initSnake.cpp
+++ src/snake/initSnake.cpp
@@ -8,7 +8,7 @@ Snake::Snake() {
     this->direction = Direction::East;
 }
 
-Snake::Snake(int length, Coordinate head, Direction direction) {
+Snake::Snake(int nRow, int nCol, int length, Coordinate head, Direction direction) {
     this->direction = direction;
     this->head = head;
     this->length = length;
@@ -17,8 +17,16 @@ Snake::Snake(int length, Coordinate head, Direction direction) {
     int dy[] = {0, -1, 0, 1};
 
     for(int i = 1; i <= length - 1; i++) {
-        Coordinate body = {head.x + i * dx[(int)direction],
-                            head.y + i * dy[(int)direction]};
+        int x = head.x + i * dx[(int)direction];
+        int y = head.y + i * dy[(int)direction];
+
+        x %= nRow;
+        y %= nCol;
+
+        if(x < 0) x += nRow;
+        if(y < 0) y += nCol;
+
+        Coordinate body = {x, y};
         
         this->body.push_back(body);
     }
