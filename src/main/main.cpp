ff68e2f09beae54a4f829b5292274dd42e861452
Andhika Rahman <arputra.me@gmail.com>
2026-03-17 20:10:48 +0700

    create mainloop for snake game

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,41 +3,94 @@
 #include "ui/ui.h"
 #include <iostream>
 
-int main() {
-    int r, c;
+#ifdef _WIN32
+    #include <conio.h>
+    #include <windows.h>
+    #define SLEEP(ms) Sleep(ms)
+#else
+    #include <termios.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #define SLEEP(ms) usleep(ms * 1000)
 
+    int _kbhit() {
+        struct termios oldt, newt;
+        int ch;
+        int oldf;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        fcntl(STDIN_FILENO, F_SETFL, oldf);
+        if(ch != EOF) {
+            ungetc(ch, stdin);
+            return 1;
+        }
+        return 0;
+    }
+
+    int _getch() {
+        struct termios oldt, newt;
+        int ch;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return ch;
+    }
+#endif
+
+int main() {
+    std::cout << "\e[?25l";
+
+    int r, c;
+    std::cout << "Enter grid size (rows cols): ";
     std::cin >> r >> c;
 
     Space * space = new Space(r, c);
+    Direction currentDir = Direction::East; 
+    bool running = true;
 
-    char direction;
+    while (running) {
+        if (_kbhit()) {
+            char key = _getch();
 
-    do {
+            Direction inputDir = currentDir; 
+            if (key == 'w')      inputDir = Direction::North;
+            else if (key == 'd') inputDir = Direction::East; 
+            else if (key == 's') inputDir = Direction::South;
+            else if (key == 'a') inputDir = Direction::West; 
+            else if (key == 'q') running = false;
+
+            if (std::abs((int)inputDir - (int)currentDir) != 2) {
+                currentDir = inputDir;
+            }
+        }
+
+        space->moveSnake(currentDir);
+        
         clearScreen();
-
         space->render();
         space->print();
 
-        std::cout << "\nEnter the direction (w, a, s, d), q to quit: ";
+        #ifdef _WIN32
+            while (_kbhit()) _getch();
+        #else
+            tcflush(STDIN_FILENO, TCIFLUSH); 
+        #endif
 
-        std::cin >> direction;
-
-        Direction newDirection;
-
-        if(direction == 'w') newDirection = Direction::North;
-        else if(direction == 'd') newDirection = Direction::East;
-        else if(direction == 's') newDirection = Direction::South;
-        else if (direction == 'a') newDirection = Direction::West;
-        else if (direction == 'q') break;
-        else {
-            std::cout << "\nInvalid direction...\n";
-            return 0;
-        }
-
-        space->moveSnake(newDirection);
-    }while (direction != 'q');
+        SLEEP(150); 
+    }
 
     delete space;
-    
+
+    std::cout << "\e[?25h";
+
     return 0;
 }
