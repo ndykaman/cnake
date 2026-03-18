5282cd2d9d90e8b95fcc9bb087ff97276c61c123
Andhika Rahman <arputra.me@gmail.com>
2026-03-19 06:03:49 +0700

    fix: change input mechanism

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,100 +5,115 @@
 #include <cmath>
 
 #ifdef _WIN32
-    #include <conio.h>
-    #include <windows.h>
-    #define SLEEP(ms) Sleep(ms)
+#include <conio.h>
+#include <windows.h>
+#define SLEEP(ms) Sleep(ms)
 #else
-    #include <termios.h>
-    #include <unistd.h>
-    #include <fcntl.h>
-    #define SLEEP(ms) usleep(ms * 1000)
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define SLEEP(ms) usleep(ms * 1000)
 
-    int _kbhit() {
-        struct termios oldt, newt;
-        int ch;
-        int oldf;
+int _kbhit()
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-        ch = getchar();
+    ch = getchar();
 
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        fcntl(STDIN_FILENO, F_SETFL, oldf);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-        if (ch != EOF) {
-            ungetc(ch, stdin);
-            return 1;
-        }
-
-        return 0;
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
     }
 
-    int _getch() {
-        struct termios oldt, newt;
-        int ch;
+    return 0;
+}
 
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+int _getch()
+{
+    struct termios oldt, newt;
+    int ch;
 
-        ch = getchar();
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        return ch;
-    }
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
 #endif
 
 // ======================= UI =======================
 
-void hideCursor() {
+void hideCursor()
+{
     std::cout << "\e[?25l";
 }
 
-void showCursor() {
+void showCursor()
+{
     std::cout << "\e[?25h";
 }
 
 // ======================= Input =======================
 
-void inputGridSize(int& r, int& c) {
+void inputGridSize(int &r, int &c)
+{
     std::cout << "Enter grid size (rows cols): ";
     std::cin >> r >> c;
 }
 
-Direction getInputDirection(Direction currentDir, bool& running) {
-    if (!_kbhit()) return currentDir;
+Direction getInputDirection(Direction currentDir, bool &running)
+{
+    while (_kbhit())
+    {
+        char key = _getch();
 
-    char key = _getch();
-    Direction inputDir = currentDir;
+        Direction inputDir = currentDir;
 
-    if (key == 'w')      inputDir = Direction::North;
-    else if (key == 'd') inputDir = Direction::East;
-    else if (key == 's') inputDir = Direction::South;
-    else if (key == 'a') inputDir = Direction::West;
-    else if (key == 'q') running = false;
+        if (key == 'w')
+            inputDir = Direction::North;
+        else if (key == 'd')
+            inputDir = Direction::East;
+        else if (key == 's')
+            inputDir = Direction::South;
+        else if (key == 'a')
+            inputDir = Direction::West;
+        else if (key == 'q')
+            running = false;
 
-    if (std::abs((int)inputDir - (int)currentDir) != 2) {
-        return inputDir;
+        if (std::abs((int)inputDir - (int)currentDir) != 2)
+        {
+            currentDir = inputDir;
+        }
     }
-
-    return currentDir;
 }
 
 // ======================= Game Loop =======================
 
-void gameLoop(Space& space) {
+void gameLoop(Space &space)
+{
     Direction currentDir = Direction::East;
     bool running = true;
 
-    while (running) {
+    while (running)
+    {
         currentDir = getInputDirection(currentDir, running);
 
         space.updateSnake(currentDir);
@@ -113,7 +128,8 @@ void gameLoop(Space& space) {
 
 // ======================= Main =======================
 
-int main() {
+int main()
+{
     hideCursor();
 
     int r, c;
