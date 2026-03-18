2489c760267767378623aad9eb6cb03713d0920b
Andhika Rahman <arputra.me@gmail.com>
2026-03-18 08:36:17 +0700

    create getter for apple class

---
+++ src/apple/getCoordinate.cpp
@@ -0,0 +1,6 @@
+#include "apple/apple.h"
+#include "coordinate/coordinate.h"
+
+Coordinate Apple::getCoordinate() {
+    return this->coordinate;
+}
---
+++ src/apple/getValue.cpp
@@ -0,0 +1,5 @@
+#include "apple/apple.h"
+
+int Apple::getValue() {
+    return this->value;
+}
