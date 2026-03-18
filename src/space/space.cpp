789047c00263df8a6366abb3215d21046ca511ba
Andhika Rahman <arputra.me@gmail.com>
2026-03-18 10:09:28 +0700

    refactor: move render function to space.cpp

--- src/space/space.cpp
+++ src/space/space.cpp
@@ -18,3 +18,39 @@ Space::Space(int nRow, int nCol) {
 
 
 
+void Space::render() {
+    std::vector<std::vector<char>> &grid = this->grid;
+
+    for(auto &vc : grid) {
+        for(auto &c : vc) {
+            c = '.';
+        }
+    }
+
+    const Coordinate &head = this->snake.getHead();
+    const Direction &direction = this->snake.getDirection();
+    const Coordinate &apple = this->apple.getCoordinate();
+
+    grid[apple.x][apple.y] = '@';
+
+    switch ((int)direction) {
+        case 0:
+            grid[head.x][head.y] = '^';
+            break;
+        case 1:
+            grid[head.x][head.y] = '>';
+            break;
+        case 2:
+            grid[head.x][head.y] = 'v';
+            break;
+        case 3:
+            grid[head.x][head.y] = '<';
+            break;
+    }
+
+    const std::vector<Coordinate> &body = this->snake.getBody();
+
+    for(auto [x,y] : body) {
+        grid[x][y] = '#';
+    }
+}
