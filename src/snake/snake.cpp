02a4172dc9f3c9901c303cc27986d9bfb83e5691
Andhika Rahman <arputra.me@gmail.com>
2026-03-18 09:42:47 +0700

    refactor: move initSnake into constructor

--- src/snake/snake.cpp
+++ src/snake/snake.cpp
@@ -1,1 +1,50 @@
 #include "snake.h"
+#include <random>
+
+const int INITIAL_SNAKE_LEN = 5;
+
+Snake::Snake() {
+    this->length = 1;
+    this->head = {0, 0};
+    this->direction = Direction::East;
+}
+
+Snake::Snake(int nRow, int nCol) {
+    this->length = INITIAL_SNAKE_LEN;
+
+    int minX = INITIAL_SNAKE_LEN - 3, maxX = nRow - INITIAL_SNAKE_LEN + 3;
+    int minY = INITIAL_SNAKE_LEN - 3, maxY = nCol - INITIAL_SNAKE_LEN + 3;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<> distX(minX, maxX);
+    std::uniform_int_distribution<> distY(minY, maxY);
+    std::uniform_int_distribution<> distD(0, 3);
+
+    int x = distX(gen);
+    int y = distY(gen);
+    int d = distD(gen);
+
+    this->head = {x, y};
+
+    this->direction = static_cast<Direction>(d);
+
+    int dx[] = {1, 0, -1, 0};
+    int dy[] = {0, -1, 0, 1};
+
+    for(int i = 1; i <= length - 1; i++) {
+        int x = head.x + i * dx[(int)direction];
+        int y = head.y + i * dy[(int)direction];
+
+        x %= nRow;
+        y %= nCol;
+
+        if(x < 0) x += nRow;
+        if(y < 0) y += nCol;
+
+        Coordinate body = {x, y};
+        
+        this->body.push_back(body);
+    }
+}
