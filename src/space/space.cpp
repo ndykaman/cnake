7ab25658211a05cb074463218fe05c26c2a6f4a6
Andhika Rahman <arputra.me@gmail.com>
2026-03-18 10:11:44 +0700

    refactor: move snake movement function to space.cpp

--- src/space/space.cpp
+++ src/space/space.cpp
@@ -68,3 +68,35 @@ void Space::print() {
         std::cout << "\n";
     }
 }
+
+
+
+void Space::moveSnake(Direction newDirection) {
+    const Coordinate &curHead = this->snake.getHead();
+    const Direction &curDirection = this->snake.getDirection();
+    const Coordinate &apple = this->apple.getCoordinate();
+    Coordinate newHead;
+
+    if(abs((int)(curDirection) - (int)newDirection) == 2) return;
+
+    int dx[] = {-1, 0, 1, 0};
+    int dy[] = {0, 1, 0, -1};
+
+    newHead = {
+        curHead.x + dx[(int)newDirection],
+        curHead.y + dy[(int)newDirection]
+    };
+
+    newHead.x %= nRow;
+    newHead.y %= nCol;
+
+    if(newHead.x < 0) newHead.x += nRow;
+    if(newHead.y < 0) newHead.y += nCol;
+
+    if(newHead.x == apple.x && newHead.y == apple.y) {
+        this->snake.eatApple();
+        this->generateRandomApple();
+    }
+
+    this->snake.move(this->nRow, this->nCol, newDirection);
+}
